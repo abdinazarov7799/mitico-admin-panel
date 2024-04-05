67691fb8bdb640c692d5cb39bc3af006aaefd5e5
Diyorbek <@diyorbek>
2024-04-05 18:04:09 +0500

    Initial commit

---
+++ src/modules/variation/components/CreateEditVariation.jsx
@@ -0,0 +1,179 @@
+import React, {useState} from 'react';
+import {useTranslation} from "react-i18next";
+import usePostQuery from "../../../hooks/api/usePostQuery.js";
+import {KEYS} from "../../../constants/key.js";
+import {URLS} from "../../../constants/url.js";
+import {Button, Form, Input, InputNumber, Select, Space} from "antd";
+import {get} from "lodash";
+import usePutQuery from "../../../hooks/api/usePutQuery.js";
+import useGetAllQuery from "../../../hooks/api/useGetAllQuery.js";
+
+const CreateEditVariation = ({itemData,setIsModalOpen,refetch}) => {
+    const { t } = useTranslation();
+    const [searchProduct, setSearchProduct] = useState(null);
+    const [searchMeasure, setSearchMeasure] = useState(null);
+    const { mutate, isLoading } = usePostQuery({
+        listKeyId: KEYS.variation_get_all,
+    });
+    const { mutate:mutateEdit, isLoading:isLoadingEdit } = usePutQuery({
+        listKeyId: KEYS.variation_get_all,
+        hideSuccessToast: false
+    });
+    const { data:products,isLoading:isLoadingProducts } = useGetAllQuery({
+        key: KEYS.product_get_all,
+        url: URLS.product_get_all,
+        params: {
+            params: {
+                search: searchProduct,
+                size: 500
+            }
+        }
+    })
+    const { data:measures,isLoading:isLoadingMeasures } = useGetAllQuery({
+        key: KEYS.measure_get_all,
+        url: URLS.measure_get_all,
+        params: {
+            params: {
+                search: searchMeasure,
+                size: 500
+            }
+        }
+    })
+    const onFinish = (values) => {
+        const formData = {
+            ...values,
+        }
+        if (itemData) {
+            mutateEdit(
+                { url: `${URLS.variation_edit}/${get(itemData,'id')}`, attributes: formData },
+                {
+                    onSuccess: () => {
+                        setIsModalOpen(false);
+                        refetch()
+                    },
+                }
+            );
+        }else {
+            mutate(
+                { url: URLS.variation_add, attributes: formData },
+                {
+                    onSuccess: () => {
+                        setIsModalOpen(false);
+                        refetch()
+                    },
+                }
+            );
+        }
+    };
+
+    return (
+        <>
+            <Form
+                onFinish={onFinish}
+                autoComplete="off"
+                layout={"vertical"}
+                initialValues={{
+                    number: get(itemData,'number'),
+                    price: get(itemData,'price'),
+                    measure: get(itemData,'measure'),
+                    nameUz: get(itemData,'nameUz'),
+                    nameRu: get(itemData,'nameRu'),
+                    productId: get(itemData,'product.id'),
+                    measureUnitId: get(itemData,'measureUnit.id'),
+                }}
+            >
+                <Form.Item
+                    label={t("Product")}
+                    name="productId"
+                    rules={[{required: true,}]}>
+                    <Select
+                        showSearch
+                        placeholder={t("Product")}
+                        optionFilterProp="children"
+                        onSearch={(e) => setSearchProduct(e)}
+                        filterOption={(input, option) =>
+                            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+                        loading={isLoadingProducts}
+                        options={get(products,'data.data.content')?.map((item) => {
+                            return {
+                                value: get(item,'id'),
+                                label: `${get(item,'nameUz')} / ${get(item,'nameRu')}`
+                            }
+                        })}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label={t("NameUz")}
+                    name="nameUz"
+                    rules={[{required: true,}]}
+                >
+                    <Input style={{width: "100%"}}/>
+                </Form.Item>
+
+                <Form.Item
+                    label={t("NameRu")}
+                    name="nameRu"
+                    rules={[{required: true,}]}
+                >
+                    <Input style={{width: "100%"}}/>
+                </Form.Item>
+
+                <Space>
+                    <Form.Item
+                        label={t("Measure")}
+                        name="measure"
+                        rules={[{required: true,}]}
+                    >
+                        <InputNumber/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label={t("Measure unit")}
+                        name="measureUnitId"
+                        rules={[{required: true,}]}>
+                        <Select
+                            showSearch
+                            placeholder={t("Measure unit")}
+                            optionFilterProp="children"
+                            onSearch={(e) => setSearchMeasure(e)}
+                            filterOption={(input, option) =>
+                                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+                            loading={isLoadingMeasures}
+                            options={get(measures,'data.data.content')?.map((item) => {
+                                return {
+                                    value: get(item,'id'),
+                                    label: `${get(item,'nameUz')} / ${get(item,'nameRu')}`
+                                }
+                            })}
+                        />
+                    </Form.Item>
+                </Space>
+
+                <Form.Item
+                    label={t("Order")}
+                    name="number"
+                    rules={[{required: true,}]}
+                >
+                    <InputNumber style={{width: "100%"}}/>
+                </Form.Item>
+
+                <Form.Item
+                    label={t("Price")}
+                    name="price"
+                    rules={[{required: true,}]}
+                >
+                    <InputNumber style={{width: "100%"}}/>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button block type="primary" htmlType="submit" loading={isLoading || isLoadingEdit}>
+                        {itemData ? t("Edit") : t("Create")}
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
+    );
+};
+
+export default CreateEditVariation;
---
+++ src/router/router.jsx
@@ -0,0 +1,104 @@
+import React, {Suspense} from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import dayjs from "dayjs";
+
+
+// LAYOUTS
+import DashboardLayout from "../layouts/dashboard/DashboardLayout.jsx";
+import AuthLayout from "../layouts/auth/AuthLayout.jsx";
+// LAYOUTS
+
+// AUTH
+import IsAuth from "../services/auth/IsAuth";
+import IsGuest from "../services/auth/IsGuest";
+import LoginPage from "../modules/auth/pages/LoginPage";
+// AUTH
+
+// 404
+import NotFoundPage from  "../modules/auth/pages/NotFoundPage";
+// 404
+
+// PAGES
+import CategoryPage from "../modules/category/pages/CategoryPage.jsx";
+import TranslationPage from "../modules/translation/pages/TranslationPage.jsx";
+import OverlayLoader from "../components/OverlayLoader.jsx";
+import ProductsPage from "../modules/products/pages/ProductsPage.jsx";
+import BannerPage from "../modules/banner/pages/BannerPage.jsx";
+import ConstantsPage from "../modules/constants/pages/ConstantsPage.jsx";
+import MeasurePage from "../modules/measure/pages/MeasurePage.jsx";
+import VariationPage from "../modules/variation/pages/VariationPage.jsx";
+import BranchPage from "../modules/branch/pages/BranchPage.jsx";
+import UsersPage from "../modules/users/pages/UsersPage.jsx";
+// PAGES
+
+
+const Router = ({ ...rest }) => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<OverlayLoader />}>
+        <IsAuth>
+          <Routes>
+            <Route path={"/"} element={<DashboardLayout />}>
+              <Route
+                  path={"/categories"}
+                  element={<CategoryPage />}
+              />
+              <Route
+                  path={"/products"}
+                  element={<ProductsPage />}
+              />
+              <Route
+                  path={"/banner"}
+                  element={<BannerPage />}
+              />
+              <Route
+                  path={"/constants"}
+                  element={<ConstantsPage />}
+              />
+              <Route
+                  path={"/measuries"}
+                  element={<MeasurePage />}
+              />
+              <Route
+                  path={"/variations"}
+                  element={<VariationPage />}
+              />
+              <Route
+                  path={"/users"}
+                  element={<UsersPage />}
+              />
+              <Route
+                  path={"/branches"}
+                  element={<BranchPage />}
+              />
+              <Route
+                  path={"/translations"}
+                  element={<TranslationPage />}
+              />
+              <Route
+                  path={"auth/*"}
+                  element={<Navigate to={"/categories"} replace />}
+              />
+              <Route
+                  path={"/"}
+                  element={<Navigate to={"/categories"} replace />}
+              />
+              <Route path={"*"} element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </IsAuth>
+
+        <IsGuest>
+          <Routes>
+            <Route path={"/auth"} element={<AuthLayout />}>
+              <Route index element={<LoginPage />} />
+            </Route>
+            <Route path={"*"} element={<Navigate to={"/auth"} replace />} />
+          </Routes>
+        </IsGuest>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
